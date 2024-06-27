e225ff1f499fcccb7054c75b9e7e4bb080393ab3
Faeq <faeqfaisal@hotmail.co.uk>
2024-06-27 18:34:17 +0100

    Completed application. Tried to add script for custom css.

---
+++ app/lib/customScript.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  var style = document.createElement("style");
+  style.innerHTML =
+    ".app-wrapper-web ._aigs {" +
+    "top: 0 !important;" +
+    "width: 100vw !important;" +
+    "height: 100vh !important;" +
+    "max-width: 100vw !important;" +
+    "margin: 0 !important;" +
+    "box-shadow: 0 !important;" +
+    "}";
+};
---
+++ app/lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+import 'package:webview_windows/webview_windows.dart';
+import 'package:window_manager/window_manager.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async {
+  // For full-screen example
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  windowManager.setTitle("WhatsApp");
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(navigatorKey: navigatorKey, home: new Scaffold(
+      //Here you can set what ever background color you need.
+      backgroundColor: Colors.white,
+      body: const ExampleBrowser()
+    ), title: 'WhatsApp');
+  }
+}
+
+class ExampleBrowser extends StatefulWidget {
+  const ExampleBrowser({super.key});
+
+  @override
+  State<ExampleBrowser> createState() => _ExampleBrowser();
+}
+
+class _ExampleBrowser extends State<ExampleBrowser> {
+  final _controller = WebviewController();
+  final _textController = TextEditingController();
+  final List<StreamSubscription> _subscriptions = [];
+  late bool _isWebviewSuspended = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    // Optionally initialize the webview environment using
+    // a custom user data directory
+    // and/or a custom browser executable directory
+    // and/or custom chromium command line flags
+    //await WebviewController.initializeEnvironment(
+    //    additionalArguments: '--show-fps-counter');
+
+    try {
+      await _controller.initialize();
+      _controller.addScriptToExecuteOnDocumentCreated('window.onload=function(){document.createElement("style").innerHTML=".app-wrapper-web ._aigs {top: 0 !important;width: 100vw !important;height: 100vh !important;max-width: 100vw !important;margin: 0 !important;box-shadow: 0 !important;}"};"');
+
+      _subscriptions.add(_controller.url.listen((url) {
+        _textController.text = url;
+      }));
+
+      _subscriptions
+          .add(_controller.containsFullScreenElementChanged.listen((flag) {
+        debugPrint('Contains fullscreen element: $flag');
+        windowManager.setFullScreen(flag);
+      }));
+
+      await _controller.setBackgroundColor(Colors.transparent);
+      await _controller.setPopupWindowPolicy(WebviewPopupWindowPolicy.deny);
+      await _controller.loadUrl('https://web.whatsapp.com');
+
+      if (!mounted) return;
+      setState(() {});
+    } on PlatformException catch (e) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: const Text('Error'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Code: ${e.code}'),
+                      Text('Message: ${e.message}'),
+                    ],
+                  ),
+                  actions: [
+                    TextButton(
+                      child: const Text('Continue'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ));
+      });
+    }
+  }
+
+  Widget compositeView() {
+    if (!_controller.value.isInitialized) {
+      return const Text(
+        'Not Initialized',
+        style: TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(0),
+        child: Column(
+          children: [
+            Card(
+              color: Colors.white,
+              elevation: 0,
+              child: Row(children: [
+                Text("  WhatsApp "),
+                Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.refresh),
+                  splashRadius: 20,
+                  onPressed: () {
+                    _controller.reload();
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.developer_mode),
+                  tooltip: 'Open DevTools',
+                  splashRadius: 20,
+                  onPressed: () {
+                    _controller.openDevTools();
+                  },
+                )
+              ]),
+            ),
+            Expanded(
+                child: Card(
+                    color: Colors.transparent,
+                    elevation: 0,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Stack(
+                      children: [
+                        Webview(
+                          _controller,
+                          permissionRequested: _onPermissionRequested,
+                        ),
+                        StreamBuilder<LoadingState>(
+                            stream: _controller.loadingState,
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData &&
+                                  snapshot.data == LoadingState.loading) {
+                                return const LinearProgressIndicator();
+                              } else {
+                                return const SizedBox();
+                              }
+                            }),
+                      ],
+                    ))),
+          ],
+        ),
+      );
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new SafeArea(child: Center(
+        child: compositeView(),
+      ),
+    );
+  }
+
+  Future<WebviewPermissionDecision> _onPermissionRequested(
+      String url, WebviewPermissionKind kind, bool isUserInitiated) async {
+    final decision = await showDialog<WebviewPermissionDecision>(
+      context: navigatorKey.currentContext!,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('WhatsApp permission requested'),
+        content: Text('WhatsApp has requested permission \'$kind\''),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () =>
+                Navigator.pop(context, WebviewPermissionDecision.deny),
+            child: const Text('Deny'),
+          ),
+          TextButton(
+            onPressed: () =>
+                Navigator.pop(context, WebviewPermissionDecision.allow),
+            child: const Text('Allow'),
+          ),
+        ],
+      ),
+    );
+
+    return decision ?? WebviewPermissionDecision.none;
+  }
+
+  @override
+  void dispose() {
+    for (var s in _subscriptions) {
+      s.cancel();
+    }
+    _controller.dispose();
+    super.dispose();
+  }
+}
